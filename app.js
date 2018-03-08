cbe069ed06af2adbf5948a5dd826a699cadd759f
Stephanie Cueto <smcueto@Stephanies-MacBook-Air.local>
2018-03-08 13:52:40 -0800

    adding express, stuck depnding on variables between mongodb

--- app.js
+++ app.js
@@ -1,13 +1,90 @@
 var express = require('express');
-
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
 var app = express();
 
+// var logger = function (req, res, next){
+//   console.log('Logging...');
+//   next();
+// }
+// app.use(logger);
 
+//view Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extende: false}));
+
+//set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Global Vars
+app.use(function(req, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+// Express validator middleware
+app.use(expressValidator());
+
+var users = [
+  {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@gmail.com',
+  },
+  {
+    id: 2,
+    first_name: 'Sara',
+    last_name: 'Smith',
+    email: 'ssmith@gmail.com',
+  },
+  {
+    id: 3,
+    first_name: 'Jo',
+    last_name: 'Jackson',
+    email: 'jjackson@gmail.com',
+  }
+]
 app.get('/', function(req, res){
-  res.send('Hello World');
-})
+  db.users.find(function (err, docs) {
+    res.render('index', {
+      title: 'Customers',
+      users: docs
+    });
+  });
+
+});
+
+app.post('/users/add', function(req, res){
+
+req.checkBody('first_name', 'First Name is Required').notEmpty();
+req.checkBody('last_name', 'Last Name is Required').notEmpty();
+req.checkBody('email', 'Email Name is Required').notEmpty();
+
+//validation
+var errors = req.validationErrors();
+if(errors){
+  res.render('index', {
+    title: 'Customers',
+    users: users,
+    errors: errors
+  });
+}else{
+  var newUser = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+  }
+  console.log('SUCCESS');
+}
+});
 
 app.listen(3000, function(){
   console.log('Server Started on Port 3000...');
